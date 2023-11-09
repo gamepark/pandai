e65be255371fd62392499a770280fab880af97d7
mizutismask <severinek@gmail.com>
2023-11-09 11:12:36 +0100

    fix: tigger now removes panda

--- rules/src/rules/PandaiPlayerTurnRule.ts
+++ rules/src/rules/PandaiPlayerTurnRule.ts
@@ -1,6 +1,7 @@
-import { Material, PlayerTurnRule } from '@gamepark/rules-api';
+import { ItemMove, Material, PlayerTurnRule, isMoveItem } from '@gamepark/rules-api';
 import { MaterialType } from '../material/MaterialType';
 import { LocationType } from '../material/LocationType';
+import { Memory } from './Memory';
 
 const OFFSET_INDEX = 10;
 
@@ -21,5 +22,11 @@ export abstract class PandaiPlayerTurnRule extends PlayerTurnRule {
 		return this.material(MaterialType.Panda)
 			.location(LocationType.GridSquare)
 			.id((id: number) => id % OFFSET_INDEX === this.player);
+	}
+
+	isLastPandaMovedWearingHat(){
+		const pandaMoved: ItemMove = this.remind(Memory.LastPandaMove);
+		return isMoveItem(pandaMoved) && this.material(MaterialType.Panda).index(pandaMoved.itemIndex).getItem()!.id > 10 
+	
 	}
 }
--- rules/src/rules/TigerCardRule.ts
+++ rules/src/rules/TigerCardRule.ts
@@ -7,24 +7,26 @@ import { RuleId } from './RuleId';
 import { PandaiCardType } from '../material/PandaiCardType';
 
 export class TigerCardRule extends PandaiPlayerTurnRule {
-	getPlayerMoves(): MaterialMove<number, number, number>[] {
+	onRuleStart(_move: RuleMove) {
+		console.log('TigerCardRule', _move);
 		const moves: MaterialMove[] = [];
-		this.removeNormalPandaOrChooseNewHat(moves);
-		return moves;
-	}
 
-	removeNormalPandaOrChooseNewHat(moves: MaterialMove[]) {
 		const move: ItemMove = this.remind(Memory.LastPandaMove);
 		if (isMoveItem(move) && move.itemType === MaterialType.Panda) {
-			if (move.itemType > 10) {
+			if (this.isLastPandaMovedWearingHat()) {
 				console.log('hat panda');
 				//hat panda
 				moves.push(this.rules().startRule(RuleId.ChooseNewHatPandaRule));
 			} else {
-				this.material(MaterialType.Panda).index(move.itemIndex).moveItem({ type: LocationType.PandaStock, player: this.player });
+				moves.push(this.material(MaterialType.Panda).index(move.itemIndex).moveItem({ type: LocationType.PandaStock, player: this.player }));
 				moves.push(this.rules().startPlayerTurn(RuleId.MovePanda, this.nextPlayer));
 			}
 		}
+		return moves;
+	}
+
+	getPlayerMoves(): MaterialMove<number, number, number>[] {
+		return [];
 	}
 }
 
@@ -32,9 +34,9 @@ export class WhiteTigerCardRule extends TigerCardRule {
 	onRuleStart(_move: RuleMove) {
 		console.log('WhiteTigerCardRule', _move);
 		const moves: MaterialMove[] = [];
-		this.removeNormalPandaOrChooseNewHat(moves);
-		this.material(MaterialType.ForestCard).id(PandaiCardType.WHITE_TIGER).moveItem({ type: LocationType.ForestDeck });
-		this.material(MaterialType.ForestCard).shuffle();
+		moves.push(this.material(MaterialType.ForestCard).id(PandaiCardType.WHITE_TIGER).moveItem({ type: LocationType.ForestDeck }));
+		//moves.push(this.material(MaterialType.ForestCard).shuffle());//todo recacher la carte avant de mélanger
+		moves.push(...super.onRuleStart(_move));
 		return moves;
 	}
 }
