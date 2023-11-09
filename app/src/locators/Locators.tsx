12c4f62137f07663cba35bf4a8e5c2a93d7eb4da
mizutismask <severinek@gmail.com>
2023-11-09 11:11:44 +0100

    feat: add token earned locator

--- app/src/locators/Locators.tsx
+++ app/src/locators/Locators.tsx
@@ -7,11 +7,13 @@ import { PandaiTokenStockLocator } from './PandaiTokenStockLocator';
 import { MeadowDeckLocator } from './MeadowDeckLocator';
 import { PandaStockLocator } from './PandaStockLocator';
 import { GridSquareLocator } from './GridSquareLocator';
+import { PandaiTokenEarnedStockLocator } from './PandaiTokenEarnedStockLocator';
 
 export const Locators: Partial<Record<LocationType, ItemLocator<PlayerColor, MaterialType, LocationType>>> = {
     [LocationType.ForestDeck]: new ForestDeckLocator(),
     [LocationType.MeadowDeck]: new MeadowDeckLocator(),
     [LocationType.PandaiTokenStock]: new PandaiTokenStockLocator(),
+    [LocationType.PandaiTokenEarnedStock]: new PandaiTokenEarnedStockLocator(),
     [LocationType.PandaStock]: new PandaStockLocator(),
     [LocationType.GridSquare]: new GridSquareLocator(),
 };
