2457a5c009021988778220acb6158a437008afd3
mizutismask <severinek@gmail.com>
2023-11-11 09:28:32 +0100

    feat: end of game if last player having pandas

--- rules/src/rules/PandaiPlayerTurnRule.ts
+++ rules/src/rules/PandaiPlayerTurnRule.ts
@@ -1,32 +1,31 @@
-import { ItemMove, Material, PlayerTurnRule, isMoveItem } from '@gamepark/rules-api';
-import { MaterialType } from '../material/MaterialType';
-import { LocationType } from '../material/LocationType';
-import { Memory } from './Memory';
+import { ItemMove, Material, PlayerTurnRule, isMoveItem } from '@gamepark/rules-api'
+import { LocationType } from '../material/LocationType'
+import { MaterialType } from '../material/MaterialType'
+import { Memory } from './Memory'
 
-const OFFSET_INDEX = 10;
+const OFFSET_INDEX = 10
 
 export abstract class PandaiPlayerTurnRule extends PlayerTurnRule {
 	getHatPanda(): Material {
 		return this.material(MaterialType.Panda)
 			.location(LocationType.GridSquare)
-			.id((id: number) => id === OFFSET_INDEX + this.player);
+			.id((id: number) => id === OFFSET_INDEX + this.player)
 	}
 
 	getNormalPandas(): Material {
 		return this.material(MaterialType.Panda)
 			.location(LocationType.GridSquare)
-			.id((id: number) => id % OFFSET_INDEX === this.player && id < OFFSET_INDEX);
+			.id((id: number) => id % OFFSET_INDEX === this.player && id < OFFSET_INDEX)
 	}
 
-	getAllPandas(): Material {
+	getAllPandas(player: number = this.player): Material {
 		return this.material(MaterialType.Panda)
 			.location(LocationType.GridSquare)
-			.id((id: number) => id % OFFSET_INDEX === this.player);
+			.id((id: number) => id % OFFSET_INDEX === player)
 	}
 
-	isLastPandaMovedWearingHat(){
-		const pandaMoved: ItemMove = this.remind(Memory.LastPandaMove);
-		return isMoveItem(pandaMoved) && this.material(MaterialType.Panda).index(pandaMoved.itemIndex).getItem()!.id > 10 
-	
+	isLastPandaMovedWearingHat() {
+		const pandaMoved: ItemMove = this.remind(Memory.LastPandaMove)
+		return isMoveItem(pandaMoved) && this.material(MaterialType.Panda).index(pandaMoved.itemIndex).getItem()!.id > 10
 	}
 }
--- rules/src/rules/TigerCardRule.ts
+++ rules/src/rules/TigerCardRule.ts
@@ -1,42 +1,50 @@
-import { ItemMove, MaterialMove, RuleMove, isMoveItem } from '@gamepark/rules-api';
-import { LocationType } from '../material/LocationType';
-import { MaterialType } from '../material/MaterialType';
-import { Memory } from './Memory';
-import { PandaiPlayerTurnRule } from './PandaiPlayerTurnRule';
-import { RuleId } from './RuleId';
-import { PandaiCardType } from '../material/PandaiCardType';
+import { ItemMove, MaterialMove, RuleMove, isMoveItem } from '@gamepark/rules-api'
+import { LocationType } from '../material/LocationType'
+import { MaterialType } from '../material/MaterialType'
+import { Memory } from './Memory'
+import { PandaiPlayerTurnRule } from './PandaiPlayerTurnRule'
+import { RuleId } from './RuleId'
+import { PandaiCardType } from '../material/PandaiCardType'
 
 export class TigerCardRule extends PandaiPlayerTurnRule {
 	onRuleStart(_move: RuleMove) {
-		console.log('TigerCardRule', _move);
-		const moves: MaterialMove[] = [];
+		console.log('TigerCardRule', _move)
+		const moves: MaterialMove[] = []
 
-		const move: ItemMove = this.remind(Memory.LastPandaMove);
+		const move: ItemMove = this.remind(Memory.LastPandaMove)
 		if (isMoveItem(move) && move.itemType === MaterialType.Panda) {
 			if (this.isLastPandaMovedWearingHat()) {
-				console.log('hat panda');
+				console.log('hat panda')
 				//hat panda
-				moves.push(this.rules().startRule(RuleId.ChooseNewHatPandaRule));
+				moves.push(this.rules().startRule(RuleId.ChooseNewHatPandaRule))
 			} else {
-				moves.push(this.material(MaterialType.Panda).index(move.itemIndex).moveItem({ type: LocationType.PandaStock, player: this.player }));
-				moves.push(this.rules().startPlayerTurn(RuleId.MovePanda, this.nextPlayer));
+				moves.push(this.material(MaterialType.Panda).index(move.itemIndex).moveItem({ type: LocationType.PandaStock, player: this.player }))
+				if (this.onlyOnePlayerHasPanda()) {
+					moves.push(this.rules().endGame())
+				} else {
+					moves.push(this.rules().startPlayerTurn(RuleId.MovePanda, this.nextPlayer))
+				}
 			}
 		}
-		return moves;
+		return moves
 	}
 
 	getPlayerMoves(): MaterialMove<number, number, number>[] {
-		return [];
+		return []
+	}
+
+	onlyOnePlayerHasPanda(): boolean {
+		return this.rules().game.players.reduce((acc, p) => acc != this.getAllPandas(p).length > 0, false)
 	}
 }
 
 export class WhiteTigerCardRule extends TigerCardRule {
 	onRuleStart(_move: RuleMove) {
-		console.log('WhiteTigerCardRule', _move);
-		const moves: MaterialMove[] = [];
-		moves.push(this.material(MaterialType.ForestCard).id(PandaiCardType.WHITE_TIGER).moveItem({ type: LocationType.ForestDeck }));
-		//moves.push(this.material(MaterialType.ForestCard).shuffle());//todo recacher la carte avant de mélanger
-		moves.push(...super.onRuleStart(_move));
-		return moves;
+		console.log('WhiteTigerCardRule', _move)
+		const moves: MaterialMove[] = []
+		moves.push(this.material(MaterialType.ForestCard).id(PandaiCardType.WHITE_TIGER).moveItem({ type: LocationType.ForestDeck }))
+		this.material(MaterialType.ForestCard).shuffle()
+		moves.push(...super.onRuleStart(_move))
+		return moves
 	}
 }
