99d6648a014c65aaf97e7aae71efa7341e03ea46
mizutismask <severinek@gmail.com>
2023-11-11 09:27:55 +0100

    feat: end of game if someone has 4 pandai tokens

--- rules/src/rules/PandaiCardRule.ts
+++ rules/src/rules/PandaiCardRule.ts
@@ -21,7 +21,11 @@ export class PandaiCardRule extends PandaiPlayerTurnRule {
 			moves.push(this.material(MaterialType.PandaiToken).location(LocationType.PandaiTokenStock).moveItem({type:LocationType.PandaiTokenEarnedStock, player:this.player}));
 			moves.push(this.material(MaterialType.Panda).location(LocationType.PandaStock).player(this.player).moveItem({...pandaMoved.location, player:this.player}));
 		}
-		moves.push(this.rules().startPlayerTurn(RuleId.MovePanda, this.nextPlayer))
+		if(this.material(MaterialType.PandaiToken).location(LocationType.PandaiTokenEarnedStock).player(this.player).length===4){
+			moves.push(this.rules().endGame());
+		}else{
+			moves.push(this.rules().startPlayerTurn(RuleId.MovePanda, this.nextPlayer))
+		}
 		return moves;
 	}
 }
