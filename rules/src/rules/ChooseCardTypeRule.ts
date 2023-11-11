a43d06d3540ee8a1629816a867a3e689b0e7b515
mizutismask <severinek@gmail.com>
2023-11-11 09:26:18 +0100

    fix: can deal cards only if there’s any left

--- rules/src/rules/ChooseCardTypeRule.ts
+++ rules/src/rules/ChooseCardTypeRule.ts
@@ -1,29 +1,34 @@
-import { ItemMove, Location, MaterialMove } from '@gamepark/rules-api';
-import { LocationType } from '../material/LocationType';
-import { MaterialType } from '../material/MaterialType';
-import { Memory } from './Memory';
-import { PandaiPlayerTurnRule } from './PandaiPlayerTurnRule';
-import { cardRuleAssoc } from './RuleId';
+import { ItemMove, Location, MaterialMove } from '@gamepark/rules-api'
+import { LocationType } from '../material/LocationType'
+import { MaterialType } from '../material/MaterialType'
+import { Memory } from './Memory'
+import { PandaiPlayerTurnRule } from './PandaiPlayerTurnRule'
+import { cardRuleAssoc } from './RuleId'
 
 export class ChooseCardTypeRule extends PandaiPlayerTurnRule {
 	getPlayerMoves(): MaterialMove<number, number, number>[] {
-		return [
-			this.material(MaterialType.ForestCard).location(LocationType.ForestDeck).deck().dealOne(this.remind(Memory.LastPandaMove).location),
-			this.material(MaterialType.MeadowCard).location(LocationType.MeadowDeck).deck().dealOne(this.remind(Memory.LastPandaMove).location),
-		];
+		const moves: MaterialMove[] = []
+
+		if (this.material(MaterialType.ForestCard).location(LocationType.ForestDeck).getItems().length > 0) {
+			moves.push(this.material(MaterialType.ForestCard).location(LocationType.ForestDeck).deck().dealOne(this.remind(Memory.LastPandaMove).location))
+		}
+		if (this.material(MaterialType.MeadowCard).location(LocationType.MeadowDeck).getItems().length > 0) {
+			this.material(MaterialType.MeadowCard).location(LocationType.MeadowDeck).deck().dealOne(this.remind(Memory.LastPandaMove).location)
+		}
+		return moves
 	}
 
 	afterItemMove(_move: ItemMove): MaterialMove[] {
 		//console.log('ChooseCardType');
-		const moves: MaterialMove[] = [];
-		const card = this.getCardOnSquare(this.remind(Memory.LastPandaMove).location);
-		this.memorize(Memory.LastCardDrawn, card);
+		const moves: MaterialMove[] = []
+		const card = this.getCardOnSquare(this.remind(Memory.LastPandaMove).location)
+		this.memorize(Memory.LastCardDrawn, card)
 		//console.log("card",card);
-		
-		moves.push(this.rules().startRule(cardRuleAssoc[card?.id]));
+
+		moves.push(this.rules().startRule(cardRuleAssoc[card?.id]))
 		//moves.push(this.rules().startPlayerTurn(RuleId.PlayerTurn, this.nextPlayer));
 
-		return moves;
+		return moves
 	}
 
 	getCardOnSquare(location: Location) {
@@ -36,6 +41,6 @@ export class ChooseCardTypeRule extends PandaiPlayerTurnRule {
 				.location(LocationType.GridSquare)
 				.filter((c) => c.location.x === location.x && c.location.y === location.y)
 				.getItem()
-		);
+		)
 	}
 }
