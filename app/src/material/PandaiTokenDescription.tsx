75932d9e8c56e74eaf8ac9fe035ac76117117be8
mizutismask <severinek@gmail.com>
2023-11-13 17:20:18 +0100

    style(pandaiTokens): sets the background color

--- app/src/material/PandaiTokenDescription.tsx
+++ app/src/material/PandaiTokenDescription.tsx
@@ -1,44 +1,51 @@
-import { RoundTokenDescription } from '@gamepark/react-game';
+import { RoundTokenDescription } from '@gamepark/react-game'
 /*import { MaterialContext } from '@gamepark/react-game'
 import { Location } from '@gamepark/rules-api'
 import { MaterialType } from '@gamepark/pandai/material/MaterialType'
 import { LocationType } from '@gamepark/pandai/material/LocationType'
 import { css } from '@emotion/react'*/
 
-import card1 from '../images/tokens/token-blue.png';
-import card2 from '../images/tokens/token-brown.png';
-import card3 from '../images/tokens/token-green.png';
-import card4 from '../images/tokens/token-purple.png';
-import card5 from '../images/tokens/token-red.png';
-import card6 from '../images/tokens/token-yellow.png';
-import { PandaiColor as PandaiColor } from '@gamepark/pandai/material/PandaiColor';
-import { css } from '@emotion/react';
+import { css } from '@emotion/react'
+import { PandaiColor } from '@gamepark/pandai/material/PandaiColor'
+import { Location } from '@gamepark/rules-api'
+import card1 from '../images/tokens/token-blue.png'
+import card2 from '../images/tokens/token-brown.png'
+import card3 from '../images/tokens/token-green.png'
+import card4 from '../images/tokens/token-purple.png'
+import card5 from '../images/tokens/token-red.png'
+import card6 from '../images/tokens/token-yellow.png'
 
 export class PandaiTokenDescription extends RoundTokenDescription {
-    ratio = 1;
-    diameter=3;
-    width=3;
-    images = {
-        [PandaiColor.Blue]: card1,
-        [PandaiColor.Brown]: card2,
-        [PandaiColor.Green]: card3,
-        [PandaiColor.Purple]: card4,
-        [PandaiColor.Red]: card5,
-        [PandaiColor.Yellow]: card6,
-    };
-//add ${color}
-    extraCss = css`
-    background: white
-  `
-   /* getExtraCss(location: Location<PandaColor, LocationType>, { rules }: MaterialContext<PandaColor, MaterialType, LocationType>) {
-        return borderCss(rules.material(MaterialType.PandaiToken)[location.id])
-      }*/
-    
+	ratio = 1
+	diameter = 5
+	width = this.diameter
+	images = {
+		[PandaiColor.Blue]: card1,
+		[PandaiColor.Brown]: card2,
+		[PandaiColor.Green]: card3,
+		[PandaiColor.Purple]: card4,
+		[PandaiColor.Red]: card5,
+		[PandaiColor.Yellow]: card6
+	}
+	//extraCss = css`background-color:red;`;
+
+	getExtraCss(location: Location) {
+		const backgroundColor = pandaiColorCode[location.id]
+		return backgroundColor && backgroundCss(backgroundColor)
+	}
 }
 
-export const pandaiTokenDescription = new PandaiTokenDescription();
-/*
-const borderCss = (color: string) => css`
-  border: 0.2em solid ${color}
+export const pandaiTokenDescription = new PandaiTokenDescription()
+
+const backgroundCss = (color: string) => css`
+  background-color: ${color}
 `
-*/
+
+const pandaiColorCode: Record<PandaiColor, string> = {
+	[PandaiColor.Red]: '#f46450',
+	[PandaiColor.Brown]: '#7a6148',
+	[PandaiColor.Blue]: '#73b4e6',
+	[PandaiColor.Green]: '#85d050',
+	[PandaiColor.Yellow]: '#fab039',
+	[PandaiColor.Purple]: '#af6ebe'
+}
